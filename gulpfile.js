d231fc1c2e8282c8c137e9daeb8945824fcda7b1
Eduardo Muramoto <dudmuramoto@gmail.com>
2018-02-21 12:12:17 -0800

    add livereload,minify,concat and autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,27 @@
 const gulp = require('gulp');
 const uglify = require('gulp-uglify-es').default;
+const livereload = require('gulp-livereload');
+const concat = require('gulp-concat');
+const minifyCss = require('gulp-minify-css');
+const autoprefixer = require('gulp-autoprefixer');
 
 //File paths
+const DIST_PATH = 'public/dist'
 const SCRIPTS_PATH = 'public/scripts/**/*.js'
+const CSS_PATH = 'public/css/**/*.css'
 
 // Styles
 gulp.task('styles',() => {
   console.log('Starting styles task');
+
+  return gulp.src(['public/css/reset.css',CSS_PATH])
+      .pipe(autoprefixer())
+      .pipe(concat('styles.css'))
+      .pipe(minifyCss())
+      .pipe(gulp.dest(DIST_PATH))
+      .pipe(livereload());
 });
+
 // Scripts
 gulp.task('scripts',() => {
   console.log('Starting scripts task');
@@ -17,21 +31,26 @@ gulp.task('scripts',() => {
      // .pipe(uglify().on('error', function(e){
      //       console.log(e);
      //    }))
-      .pipe(gulp.dest('public/dist'));
+      .pipe(gulp.dest(DIST_PATH))
+      .pipe(livereload());
 });
+
 // Images
 gulp.task('images',() => {
   console.log('Starting images task');
+});
+
+
+// Default
+gulp.task('default',() => {
+  console.log('Starting default task');
 });
 
 // Watch
 gulp.task('watch',() => {
   console.log('Starting watch task');
   require('./server.js');
+  livereload.listen();
   gulp.watch(SCRIPTS_PATH, ['scripts']);
-});
-
-// Default
-gulp.task('default',() => {
-  console.log('Starting default task');
+  gulp.watch(CSS_PATH, ['styles']);
 });
